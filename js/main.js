6f489eb9a0fd0e56e931926a7672f5f101d8cbe5
webmanufactory <bubunya@yandex.ru>
2020-05-02 21:24:17 +0300

    new js code

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 $(function(){
-    
-    $('.load-more'.on('click', function(){
-        const btn = $(this);
-        const loader = btn.find('span');
-        $.ajax({
-           url: '/data.html',
-            type: 'POST',
-            boforeSend: function(){
-                btn.attr('disbled', true);
-                loader.addClass('d-inline-block');
-            },
-            success: function(responce){
-                setTimeout(function(){
-                    loader.removeClass('d-inline-block');
-                    btn.attr('disabled', false);
-                    $('.after-posts').before(responce);
-                    console.log(responce);
-                           }, 1000);
-            },
-            error: function(){
-                alert('Error!');
-                loader.removeClass('d-inline-block');
-                btn.attr('disabled', false);
-            }
-        });
-    });   
+
+	$('.load-more').on('click', function(){
+		const btn = $(this);
+		const loader = btn.find('span');
+		$.ajax({
+			url: '/data.html',
+			type: 'GET',
+			beforeSend: function(){
+				btn.attr('disabled', true);
+				loader.addClass('d-inline-block');
+			},
+			success: function(responce){
+				setTimeout(function(){
+					loader.removeClass('d-inline-block');
+					btn.attr('disabled', false);
+					$('.after-posts').before(responce);
+				}, 1000);
+			},
+			error: function(){
+				alert('Error!');
+				loader.removeClass('d-inline-block');
+				btn.attr('disabled', false);
+			}
+		});
+	});
+
 });
